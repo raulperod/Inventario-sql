e94d837faa4fba25097031ab89661f6d9e9bdeed
Raul Perez <raulperod@gmail.com>
2017-04-21 16:40:03 -0700

    termianda la ruta categories

--- controllers/category.js
+++ controllers/category.js
@@ -6,13 +6,12 @@
 const CategoryModel = require('../models/category')
 
 function categoriesGet(req, res) {
-    let seleccion = ['*']
     // busco todas las categorias
-    CategoryModel.getCategories(seleccion, categorias => { // si no hubo error
+    CategoryModel.getCategories( categorias => { // si no hubo error
         res.render('./categories/manager', { categorias, usuario: req.session.user })
     }, error => { // si ocurrio un error
         console.log(`Error al obtener las categorias: ${error}`)
-        res.redirect('/almacen')
+        res.json({ msg: `Error al obtener las categorias: ${error}`, tipo: 0})
     })
 }
 
@@ -24,43 +23,42 @@ function categoriesNewPost(req, res) {
     // creo la nueva categoria
     let nuevaCategoria = {
         nombre: req.body.nombre,
-        description: req.body.description
+        descripcion: req.body.descripcion
     }
     // guardo la nueva categoria
     CategoryModel.createCategory(nuevaCategoria, () => { // si no hubo error
-        res.redirect('/categorias')
+        res.redirect('/categories')
     }, error => { // si ocurrio un error
         console.log(`Error al agregar la nueva categoria: ${error}`)
         // mando una alerta que ya existe ese nombre
-        res.send('0')
+        res.json({ msg: `Error al agregar la nueva categoria: ${error}`, tipo: 1})
     })
 }
 
 function categoriesIdCategoryGet(req, res) {
-    let seleccion = ['*'],
-        idCategory = req.params.idCategory
+    let idCategory = req.params.idCategory
     // busco la categoria
-    CategoryModel.getCategoryById(idCategory, seleccion, categoriaUpdate => { // si no hubo error
-        res.render('./categories/update', { usuario: req.session.user, categoriaUpdate: categoriaUpdate[0] })
+    CategoryModel.getCategoryById(idCategory, categoriaUpdate => { // si no hubo error
+        res.render('./categories/update', { usuario: req.session.user, categoryUpdate: categoriaUpdate[0] })
     }, error => { // si hubo un error
         console.log(`Error al obtener la categoria: ${error}`)
-        res.redirect('/almacen')
+        res.json({ msg: `Error al obtener la categoria: ${error}`, tipo: 0})
     })
 }
 
 function categoriesIdCategoryPut(req, res) {
     let categoriaUpdate = {
-            idCategoria: req.params.idCategory,
-            nombre: req.body.nombre,
-            descripcion: req.body.descripcion
-        }
+        idCategoria: req.params.idCategory,
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion
+    }
     // actualizo la categoria en la base de datos
     CategoryModel.updateCategory(categoriaUpdate, () => { // si no hubo error
         res.redirect('/categories')
     }, error => { // si hubo error
         console.log(`Error al actualizar la categoria: ${error}`)
         // como se repite el nombre, se manda una alerta
-        res.send('0')
+        res.json({ msg: `Error al actualizar la categoria: ${error}`, tipo: 1})
     })
 
 }
--- models/category.js
+++ models/category.js
@@ -5,8 +5,8 @@
 
 const CategoryModel = require('./coneccion')
 
-function getCategoryById(idCategory, seleccion, render, printError) {
-    CategoryModel.query('SELECT ?? FROM categorias WHERE idCategoria = ?', [seleccion, idCategory], (error, resultado, fields) => {
+function getCategoryById(idCategory, render, printError) {
+    CategoryModel.query('SELECT * FROM categorias WHERE idCategoria = ?', idCategory, (error, resultado, fields) => {
         return(error) ? printError(error): render(resultado)
     })
 }
@@ -17,8 +17,8 @@ function getCategoryByName(name, seleccion, render, printError) {
     })
 }
 
-function getCategories(seleccion, render, printError) {
-    CategoryModel.query('SELECT ?? FROM categorias', seleccion , (error, resultado, fields) => {
+function getCategories( render, printError) {
+    CategoryModel.query('SELECT * FROM categorias' , (error, resultado, fields) => {
         return(error) ? printError(error): render(resultado)
     })
 }
@@ -30,7 +30,7 @@ function createCategory(category, render, printError) {
 }
 
 function updateCategory(category, render, printError) {
-    CategoryModel.query('UPDATE categorias SET ? WHERE idCategoria = ?', [category,category.idCategory], (error, resultado, fields) => {
+    CategoryModel.query('UPDATE categorias SET ? WHERE idCategoria = ?', [category,category.idCategoria], (error, resultado, fields) => {
         return(error) ? printError(error): render()
     })
 }
