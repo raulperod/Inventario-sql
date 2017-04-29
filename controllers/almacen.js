02ce3dee2589d3781ac86a5b5d354218570d18fc
Raul Perez <raulperod@gmail.com>
2017-04-28 19:49:33 -0700

    obtiene el almacen

--- controllers/almacen.js
+++ controllers/almacen.js
@@ -3,9 +3,23 @@
  */
 'use strict'
 
+const AlmacenModel = require('../models/almacen'),
+      Utilidad = require('../ayuda/utilidad')
 
 function almacenGet(req, res) {
+    let usuario = req.session.user
+    if( usuario.permisos === 2){ // si es administrador general
+        // obtengo el almacen
+        AlmacenModel.getAlmacenForGeneralAdmin((error, almacen) => {
+            if(error){
+                Utilidad.printError(res, {msg: `Error al obtener el almacen: ${error}`, tipo: 0})
+            } else {
+                res.render('./almacen/manager', { usuario, almacen})
+            }
+        })
+    }else{ // si es administrador de sucursal o recepcionista
 
+    }
 }
 
 function almacenNewGet(req, res) {
--- models/almacen.js
+++ models/almacen.js
@@ -5,6 +5,30 @@
 
 const AlmacenModel = require('./coneccion')
 
+function getAlmacenForGeneralAdmin(next) {
+    AlmacenModel
+        .query(`SELECT a.cantidadAlmacen, p.nombre nombreProducto, p.codigo, c.nombre nombreCategoria, s.plaza 
+                FROM almacen a 
+                JOIN productos p on a.idProducto = p.idProducto
+                JOIN categorias c on a.idCategoria = c.idCategoria
+                JOIN sucursales s on a.idSucursal = s.idSucursal`
+                , (error, resultado, fields) => {
+            next(error, resultado)
+    })
+}
+
+function getAlmacenForSucursalAdmin(idSucursal ,next) {
+    AlmacenModel
+        .query(`SELECT p.nombre nombreProducto, p.codigo, c.nombre nombreCategoria, a.cantidadAlmacen
+                FROM almacen a
+                JOIN productos p ON a.idProducto = p.idProducto
+                JOIN categorias c ON a.idCategoria = c.idCategoria
+                WHERE a.idSucursal = ?`
+            , idSucursal ,(error, resultado, fields) => {
+                next(error, resultado)
+            })
+}
+
 function createAlmacen(almacen, next) {
     AlmacenModel.query('INSERT INTO almacen SET ?', almacen, (error, resultado, fields) => {
         next(error)
@@ -12,5 +36,7 @@ function createAlmacen(almacen, next) {
 }
 
 module.exports = {
+    getAlmacenForGeneralAdmin,
+    getAlmacenForSucursalAdmin,
     createAlmacen
 }
