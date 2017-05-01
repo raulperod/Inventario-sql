c858fe4a1adc77a989a538f71b3b9c9596f118b2
Raul Perez <raulperod@gmail.com>
2017-05-01 12:41:15 -0700

    correciones

--- models/basico.js
+++ models/basico.js
@@ -6,8 +6,8 @@
 const BasicoModel = require('./coneccion')
 
 function getBasicoByProductAndTecnica(idProducto,idTecnica, next) {
-    BasicoModel.query(`SELECT b.enUso FROM basicosenuso b WHERE b.idTecnica = ? AND b.idProducto = ?`, [idProducto,idTecnica], (error, resultado, fields) => {
-        next(error, resultado)
+    BasicoModel.query(`SELECT b.enUso FROM basicosenuso b WHERE b.idProducto = ? AND b.idTecnica = ?`, [idProducto, idTecnica], (error, resultado, fields) => {
+        next(error, resultado[0])
     })
 }
 
