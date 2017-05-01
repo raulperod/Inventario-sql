acf95e50e9c9e914c33c821000d1241b27a53ee0
Raul Perez <raulperod@gmail.com>
2017-05-01 15:12:40 -0700

    actualizacion

--- controllers/historial.js
+++ controllers/historial.js
@@ -3,20 +3,37 @@
  */
 'use strict'
 
-function historialMovimientosGet(req, res) {
+const MovimientoModel = require('../models/movimiento'),
+      BajaModel = require('../models/baja')
 
+function historialMovimientosGet(req, res) {
+    let usuario = req.session.user
+    if( usuario.permisos === 1){ // si es administrador de sucursales
+        // obtener los movimientos de productos basicos y no basicos de la sucursal
+
+    }else{ // si es administrador general
+        // obtener los movimientos de productos basicos y no basicos de todas las sucursales
+
+    }
 }
 
 function historialBajasGet(req, res) {
+    let usuario = req.session.user
+    if( usuario.permisos === 1){ // si es administrador de sucursal
+        // obtener las bajas de productos basicos y no basicos de la sucursal
 
+    }else{ // si es administrador general
+        // obtener las bajas de productos basicos y no basicos de todas las sucursales
+
+    }
 }
 
 function historialGeneralGet(req, res) {
-
+    res.redirect('/almacen')
 }
 
 function historialSucursalGet(req, res) {
-
+    res.redirect('/almacen')
 }
 
 function historialDatosGeneralGet(req, res) {
--- models/movimiento.js
+++ models/movimiento.js
@@ -5,6 +5,33 @@
 
 const MovimientoModel = require('./coneccion')
 
+function getMovimientosNoBasicos(next) {
+    MovimientoModel.query(`SELECT p.nombre nombreProducto, m.cantidad, m.tipo, concat(u.nombre,' ',u.apellido) nombreUsuario, s.plaza, m.fecha
+                           FROM movimientosnobasicos m
+                           JOIN  
+                            `, movimiento, (error, resultado, fields) => {
+        next(error, resultado)
+    })
+}
+
+function getMovimientosBasicos(next) {
+    MovimientoModel.query(``, movimiento, (error, resultado, fields) => {
+        next(error, resultado)
+    })
+}
+
+function getMovimientosNoBasicosBySucursal(idSucursal, next) {
+    MovimientoModel.query(``, idSucursal, (error, resultado, fields) => {
+        next(error, resultado)
+    })
+}
+
+function getMovimientosBasicosBySucursal(idSucursal, next) {
+    MovimientoModel.query(``, idSucursal, (error, resultado, fields) => {
+        next(error, resultado)
+    })
+}
+
 function createMovimientoNoBasico(movimiento, next) {
     MovimientoModel.query('INSERT INTO movimientosnobasicos SET ?', movimiento, (error, resultado, fields) => {
         next(error)
@@ -18,6 +45,10 @@ function createMovimientoBasico(movimiento, next) {
 }
 
 module.exports = {
+    getMovimientosNoBasicos,
+    getMovimientosBasicos,
+    getMovimientosNoBasicosBySucursal,
+    getMovimientosBasicosBySucursal,
     createMovimientoNoBasico,
     createMovimientoBasico
 }
